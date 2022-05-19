8eb135a37ba060a7c29a391690fc7e495a2f20f3
Mitchell Brown <105828845+MitchellBrownNE@users.noreply.github.com>
2022-05-18 22:27:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
-/*==========================================================
-
- Title:   Lab 7 - C++ Care Unit
- Course:  	CSC 1100
- Author:  	<First & Last Name>
- Date:    	< / / >
- Description: This program ....
-
- ==========================================================
-*/
-
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 // Write functions here
 
@@ -45,21 +33,19 @@ int swap(int w1, int w2)
     return 0;
 }
 
-// You can't edit the main file - we froze it anyway :) 
-// So all you need to do is define the four functions listed
 int main() {
     double w1, w2, h;
-    cin >> w1 >> w2 >> h;
+    std::cin >> w1 >> w2 >> h;
 
     float index1 = BMIcalculator(w1);
     float index2 = BMIcalculator(w2, conversionMeters(h));
 
-    cout << index1 << " " << index2 << endl;
+    std::cout << index1 << " " << index2 << std::endl;
 
     swap(w1, w2);
 
     index1 = BMIcalculator(w1);
     index2 = BMIcalculator(w2, conversionMeters(h));
-    cout << index1 << " " << index2 << endl;
+    std::cout << index1 << " " << index2 << std::endl;
 }
 
